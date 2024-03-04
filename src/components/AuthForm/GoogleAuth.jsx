1e96803db7f21071e3ff1a9391db2923102c784d
Olga Yudkin <cvtqx@protonmail.com>
2024-03-04 12:05:19 -0500

    google auth

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,12 +1,53 @@
 import { Flex, Image, Text } from '@chakra-ui/react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '../../firebase/firebase';
+import useShowToast from '../../hooks/useShowToast';
+import useAuthstore from '../../store/authStore';
+import { doc, setDoc } from 'firebase/firestore';
 
-const GoogleAuth = () => {
+const GoogleAuth = ({ prefix }) => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthstore((state) => state.login);
+
+  const handleGoogleAuth = async () => {
+    try {
+      const newUser = await signInWithGoogle();
+      if (!newUser && error) {
+        showToast('Error', error.message, 'error');
+        return
+      }
+
+      if (newUser) {
+        const userDoc = {
+          uid: newUser.user.uid,
+          email: newUser.user.email,
+          username: newUser.user.email.split('@')[0],
+          fullname: newUser.user.displayName,
+          bio: '',
+          profilePicURL: newUser.user.photoURL,
+          followers: [],
+          following: [],
+          posts: [],
+          createdAt: Date.now(),
+        };
+        await setDoc(doc(firestore, 'users', newUser.user.uid), userDoc);
+        //add data to local storage
+        localStorage.setItem('user-info', JSON.stringify(userDoc));
+        loginUser(userDoc);
+
+      }
+    } catch (error) {
+      showToast('Error', error.message, 'error')
+    }
+  }
+
   return (
-    <Flex alignItems={'center'} justifyContent={'center'} cursor={'pointer'}>
+    <Flex alignItems={'center'} justifyContent={'center'} cursor={'pointer'} onClick={handleGoogleAuth}>
       <Image src='google.png' w={5} alt='Google logo' />
       <Text mx='2' color={'blue.500'}>
         {' '}
-        Log in with Google
+        {prefix} with Google
       </Text>
     </Flex>
   );
--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,18 +1,26 @@
 import { Avatar, Button, Flex, Text } from '@chakra-ui/react'
 import useLogout from '../../hooks/useLogout'
 import useAuthstore from '../../store/authStore';
+import { Link } from 'react-router-dom';
 
 
 const SuggestedHeader = () => {
     const { handleLogout, isLoggingOut } = useLogout();
-    const authUser = useAuthstore(state => state.user);
+  const authUser = useAuthstore(state => state.user);
+
+  if (!authUser) return null;
+  
   return (
     <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
       <Flex alignItems={'center'} gap={2}>
-        <Avatar  size={'lg'} src={authUser.profilePicURL} />
-        <Text fontSize={12} fontWeight={'bold'}>
-          {authUser.username}
-        </Text>
+        <Link to={`${authUser.username}`}>
+          <Avatar size={'lg'} src={authUser.profilePicURL} />
+        </Link>
+        <Link to={`${authUser.username}`}>
+          <Text fontSize={12} fontWeight={'bold'}>
+            {authUser.username}
+          </Text>
+        </Link>
       </Flex>
       <Button
         size={'xs'}
