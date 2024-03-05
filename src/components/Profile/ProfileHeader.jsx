5a9f8e8692f8dbb2a6e156b08dcade8f5ce0bff7
Olga Yudkin <cvtqx@protonmail.com>
2024-03-05 16:10:08 -0500

    edit profile functioanlity

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,7 +1,15 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from '@chakra-ui/react'
+import useUserProfileStore from '../../store/userProfileStore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import useAuthstore from '../../store/authStore';
+import EditProfile from './EditProfile';
 
 const ProfileHeader = () => {
+  const { userProfile } = useUserProfileStore();
+  const authUser = useAuthstore(state => state.user);
+  const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+ const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: 'column', sm: 'row' }}>
       <AvatarGroup
@@ -10,7 +18,7 @@ const ProfileHeader = () => {
         alignSelf={'flex-start'}
         mx={'auto'}
       >
-        <Avatar name='OlgaDev' src='/profilepic.png' alt='OlgaDev profile pic' />
+        <Avatar name={userProfile.username} src={userProfile.profilePicURL} alt='Userprofile pic' />
       </AvatarGroup>
       <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
         <Flex
@@ -20,45 +28,61 @@ const ProfileHeader = () => {
           alignItems={'center'}
           w={'full'}
         >
-          <Text fontSize={{ base: 'sm', md: 'lg' }}>OlgaDev</Text>
-          <Flex gap={4} justifyContent={'center'} alignItems={'center'}>
-            <Button
-              bg={'white'}
-              color={'black'}
-              _hover={{ bg: 'whiteAlpha.800' }}
-              size={{ base: 'xs', md: 'sm' }}
-            >
-              Edit Profile
-            </Button>
-          </Flex>
+          <Text fontSize={{ base: 'sm', md: 'lg' }}>{userProfile.username}</Text>
+          {visitingOwnProfileAndAuth && (
+            <Flex gap={4} justifyContent={'center'} alignItems={'center'}>
+              <Button
+                bg={'white'}
+                color={'black'}
+                _hover={{ bg: 'whiteAlpha.800' }}
+                size={{ base: 'xs', md: 'sm' }}
+                onClick={onOpen}
+              >
+                Edit Profile
+              </Button>
+            </Flex>
+          )}
+          {visitingAnotherProfileAndAuth && (
+            <Flex gap={4} justifyContent={'center'} alignItems={'center'}>
+              <Button
+                bg={'blue.500'}
+                color={'white'}
+                _hover={{ bg: 'blue.600' }}
+                size={{ base: 'xs', md: 'sm' }}
+              >
+                Follow
+              </Button>
+            </Flex>
+          )}
         </Flex>
         <Flex alignItems={'center'} gap={{ base: 2, sm: 4 }}>
           <Text fontSize={{ base: 'xs', md: 'sm' }}>
             <Text as='span' fontWeight={'bold'} mr={1}>
-              4
+              {userProfile.posts.length}
             </Text>
             Posts
           </Text>
           <Text fontSize={{ base: 'xs', md: 'sm' }}>
             <Text as='span' fontWeight={'bold'} mr={1}>
-              148
+              {userProfile.followers.length}
             </Text>
             Followers
           </Text>
           <Text fontSize={{ base: 'xs', md: 'sm' }}>
             <Text as='span' fontWeight={'bold'} mr={1}>
-              54
+              {userProfile.following.length}
             </Text>
             Following
           </Text>
         </Flex>
         <Flex alignItems={'center'} gap={4}>
           <Text fontSize={'sm'} fontWeight={'bold'}>
-            OlgaDev
+            {userProfile.fullName}
           </Text>
         </Flex>
-        <Text fontSize={'sm'}>This is an Instagram app clone</Text>
+        <Text fontSize={'sm'}>{userProfile.bio}</Text>
       </VStack>
+      {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} /> }
     </Flex>
   );
 }
