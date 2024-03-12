70b028f8d6137256cbeb9917a1c852d8a04800d6
Olga Yudkin <cvtqx@protonmail.com>
2024-03-12 10:56:12 -0400

    updated userProfile store and added poststore

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -3,7 +3,10 @@ import { create } from "zustand";
 const useUserProfileStore = create((set) => ({
     userProfile: null,
     setUserProfile: userProfile => set({ userProfile }),
-    // addPost:()
+    //this is used to update the nubmer of posts in te profile page
+    addPost: (post) => set(state => ({
+        userProfile: {...state.userProfile, posts: [post.id, ...state.userProfile.posts]}
+    })),
 }))
 
 export default useUserProfileStore;
