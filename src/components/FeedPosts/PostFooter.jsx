db789c70802af602d5ea678a021ecfe5a890b38a
Olga Yudkin <cvtqx@protonmail.com>
2024-03-15 14:33:59 -0400

    handleLIkePost functionality

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -3,35 +3,28 @@ import { useRef, useState } from 'react';
 import { NotificationsLogo, UnlikeLogo, CommentLogo } from '../../assets/constants';
 import usePostComment from '../../hooks/usePostComment';
 import useAuthstore from '../../store/authStore';
+import useLikePost from '../../hooks/useLikePost';
 
 const PostFooter = ({ post, username, isProfilePage }) => {
-  const [liked, setLiked] = useState(false);
-  const [likes, setLikes] = useState(1000);
+  
   const { isCommenting, handlePostComment } = usePostComment();
   const [comment, setComment] = useState('');
   const authUser = useAuthstore(state => state.user);
   const commentRef = useRef(null);
+  const { isLiked, likes, handleLikePost } = useLikePost(post);
 
   const handleSubmitComment = async () => {
     await handlePostComment(post.id, comment);
     setComment('');
   };
 
-  const handleLike = () => {
-    if (liked) {
-      setLiked(false);
-      setLikes(likes - 1);
-    } else {
-      setLiked(true);
-      setLikes(likes + 1);
-    }
-  };
+  
 
   return (
     <Box my={10} marginTop={'auto'}>
       <Flex alignItems={'center'} gap={4} w={'full'} pt={0} mb={2} mt={4}>
-        <Box onClick={handleLike} cursor={'pointer'} fontSize={18}>
-          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        <Box onClick={handleLikePost} cursor={'pointer'} fontSize={18}>
+          {!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
         </Box>
         <Box cursor={'pointer'} fontSize={18} onClick={() => commentRef.current.focus()}>
           <CommentLogo />
